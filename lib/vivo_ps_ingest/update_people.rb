c94cb54df2cc06c9a2546ba2a39ea3b7bfac07ef
Alex Rockwell <alexhr@ufl.edu>
2011-02-15 16:50:59 -0500

    Added code to write rdf of a person from peoplesoft.

--- lib/vivo_ps_ingest/update_people.rb
+++ lib/vivo_ps_ingest/update_people.rb
@@ -1,6 +1,10 @@
 module VivoPsIngest
   class UpdatePeople
-    # returns the difference between two  graphs
+    # returns the difference between two graphs as a hash
+    # :removals contains the graph to remove
+    # :additions contains the graph to add
+    #
+    # if there is no changes returns {}
     def difference_between_graphs(graph_a, graph_b)
       difference = {}
       if graph_a.isomorphic_with? graph_b
@@ -11,5 +15,176 @@ module VivoPsIngest
       end
       return difference
     end
+
+    # returns the graph people containing only their hr properties
+    def retrieve_person_from_vivo(ufid)
+      sparql = <<-EOH
+PREFIX rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX bibo: <http://purl.org/ontology/bibo/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX ufVivo: <http://vivo.ufl.edu/ontology/vivo-ufl/>
+PREFIX core: <http://vivoweb.org/ontology/core#>
+
+construct {
+  ?person ufVivo:ufid ?ufid .
+  ?person foaf:firstName ?first_name .
+  ?person foaf:lastName ?last_name .
+  ?person core:middleName ?middle_name .
+  ?person bibo:prefixName ?prefix_name .
+  ?person bibo:suffixName ?suffix_name .
+
+  ?person core:workPhone ?work_phone .
+  ?person core:workFax ?work_fax .
+
+  ?person core:workEmail ?work_email .
+
+  ?person rdf:type ?type
+}
+where
+{
+  ?person ufVivo:ufid "#{ufid}".
+  ?person rdf:type foaf:Person .
+
+  ?person foaf:firstName ?first_name .
+  ?person foaf:lastName ?last_name .
+  optional { ?person core:middleName ?middle_name }
+  optional { ?person bibo:prefixName ?prefix_name }
+  optional { ?person bibo:suffixName ?suffix_name }
+
+  optional { ?person core:workPhone ?work_phone }
+  optional { ?person core:workFax ?work_fax }
+
+  optional { ?person core:workEmail ?work_email }
+
+  ?person rdf:type ?type
+}
+      EOH
+
+      hostname = ENV['vivo_hostname'] 
+      username = ENV['vivo_username']
+      password = ENV['vivo_password']
+      sparql_client = VivoWebApi::Client.new(hostname)
+      sparql_client.authenticate(username, password)
+      results = sparql_client.execute_sparql_construct(username, password, sparql)
+      
+      return results
+    end
+
+    def create_rdf_for_person_in_ps(dbh, uri, ufid)
+      person = RDF::Graph.new
+      person.insert(create_work_title_rdf(dbh, uri, ufid))
+      person.insert(create_glid_rdf(dbh, uri, ufid))
+      person.insert(create_work_email_rdf(dbh, uri, ufid))
+      person.insert(create_name_rdf(dbh, uri, ufid))
+      person.insert(create_phone_number_rdf(dbh, uri, ufid))
+
+      return person
+    end
+
+    def create_work_title_rdf(dbh, uri, ufid)
+      sql = "select work_title from psIngestDev.ps_employee_records where ufid = ?"
+
+      hr_job_title_pred = RDF::URI.new('http://vitro.mannlib.cornell.edu/ns/vitro/0.7#moniker')
+
+      sth = dbh.prepare(sql)
+      sth.execute(ufid)
+      
+      graph = RDF::Graph.new
+      sth.fetch do |row|
+        graph << RDF::Statement(uri, hr_job_title_pred, row[:work_title])
+      end
+      
+      return graph
+    end
+    
+    def create_glid_rdf(dbh, uri, ufid)
+      sql = "select glid from psIngestDev.ps_glid where ufid = ?"
+
+      gatorlink_pred = RDF::URI.new('http://vivo.ufl.edu/ontology/vivo-ufl/gatorlink')
+
+      sth = dbh.prepare(sql)
+      sth.execute(ufid)
+      graph = RDF::Graph.new 
+      sth.fetch do |row|
+        graph << RDF::Statement(uri, gatorlink_pred, row[:glid])
+      end
+      return graph
+    end
+
+    def create_work_email_rdf(dbh, uri, ufid)
+      sql = "select uf_email as work_email from psIngestDev.ps_employee_records ps where ufid = ?"
+
+      work_email_pred = RDF::URI.new('http://vivoweb.org/ontology/core#workEmail')
+
+      sth = dbh.prepare(sql)
+      sth.execute(ufid)
+
+      graph = RDF::Graph.new
+      sth.fetch do |row|
+        graph << RDF::Statement(uri, work_email_pred, row[:work_email])
+      end
+      return graph
+    end
+
+    def create_name_rdf(dbh, uri, ufid)
+      sql = "select type_cd, name_text from psIngestDev.ps_names where ufid = ?"
+      first_name_pred = RDF::URI.new('http://xmlns.com/foaf/0.1/firstName')
+      last_name_pred = RDF::URI.new('http://xmlns.com/foaf/0.1/lastName')
+      middle_name_pred = RDF::URI.new('http://vivoweb.org/ontology/core#middleName') 
+      label_pred = RDF::URI.new('http://www.w3.org/2000/01/rdf-schema#label')
+      prefix_name_pred = RDF::URI.new('http://purl.org/ontology/bibo/prefixName')
+      suffix_name_pred = RDF::URI.new('http://purl.org/ontology/bibo/suffixName')
+
+      sth = dbh.prepare(sql)
+      sth.execute(ufid)
+      graph = RDF::Graph.new
+      sth.fetch do |row|
+        if row[:type_cd] == 35
+          graph << RDF::Statement(uri, first_name_pred, row[:name_text])
+        elsif row[:type_cd] == 36
+          graph << RDF::Statement(uri, last_name_pred, row[:name_text])
+        elsif row[:type_cd] == 37 && row[:type_cd] != ""
+          graph << RDF::Statement(uri, middle_name_pred, row[:name_text])
+        elsif row[:type_cd] == 38
+          graph << RDF::Statement(uri, prefix_name_pred, row[:name_text])
+        elsif row[:type_cd] == 39
+          graph << RDF::Statement(uri, suffix_name_pred, row[:name_text])
+        elsif row[:type_cd] == 232
+          graph << RDF::Statement(uri, label_pred, row[:name_text])
+        end
+      end
+      return graph
+    end
+
+    def create_phone_number_rdf(dbh, uri, ufid)
+      sql = <<-EOH
+select ufid, type_cd, area_code, extension, phone_number 
+from psIngestDev.ps_phone_numbers 
+where ufid = ?
+      EOH
+
+      work_phone_pred = RDF::URI.new('http://vivoweb.org/ontology/core#workPhone')
+      work_fax_pred = RDF::URI.new('http://vivoweb.org/ontology/core#workFax')
+
+      sth = dbh.prepare(sql)
+      sth.execute(ufid)
+      graph = RDF::Graph.new
+      sth.fetch do |row|
+        # write out phone number in format NNN.NNN.NNNN xNNNN
+        area_code = row[:area_code].nil? ? "" : row[:area_code].strip
+        phone_number = row[:phone_number].nil? ? "" : row[:phone_number].strip
+        extension = row[:extension].nil? ? "" : row[:extension].strip
+        phone = "#{area_code}.#{phone_number.slice(0..2)}.#{phone_number.slice(3..6)}"
+        phone = extension == "" ? phone : phone + " x" + extension
+
+        if row[:type_cd] == "10"
+          graph << RDF::Statement(uri, work_phone_pred, phone)
+        elsif row[:type_cd] == "11"
+          graph << RDF::Statement(uri, work_fax_pred, phone)
+        end
+      end
+      return graph
+    end
+
   end
 end
--- spec/vivo_ps_ingest/update_people_spec.rb
+++ spec/vivo_ps_ingest/update_people_spec.rb
@@ -2,6 +2,24 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 module VivoPsIngest
   describe UpdatePeople do
+    before(:each) do
+      @ufid = "81036590"
+      @uri = "http://vivo.ufl.edu/individual/n1639"
+      @predicates = {
+        :first_name => RDF::URI.new('http://xmlns.com/foaf/0.1/firstName'),
+        :last_name => RDF::URI.new('http://xmlns.com/foaf/0.1/lastName'),
+        :middle_name => RDF::URI.new('http://vivoweb.org/ontology/core#middleName') ,
+        :label => RDF::URI.new('http://www.w3.org/2000/01/rdf-schema#label'),
+        :prefix_name => RDF::URI.new('http://purl.org/ontology/bibo/prefixName'),
+        :suffix_name => RDF::URI.new('http://purl.org/ontology/bibo/suffixName'),
+        :work_phone => RDF::URI.new('http://vivoweb.org/ontology/core#workPhone'),
+        :work_fax => RDF::URI.new('http://vivoweb.org/ontology/core#workFax'),
+        :work_email => RDF::URI.new('http://vivoweb.org/ontology/core#workEmail'),
+        :hr_job_title => RDF::URI.new('http://vitro.mannlib.cornell.edu/ns/vitro/0.7#moniker'),
+        :gatorlink => RDF::URI.new('http://vivo.ufl.edu/ontology/vivo-ufl/gatorlink')
+      }
+    end
+
     it "should return difference between person_1 and person_2" do
       person_1 = RDF::Graph.load(File.dirname(__FILE__) + '/../test/person_1.nt')
       person_2 = RDF::Graph.load(File.dirname(__FILE__) + '/../test/person_2.nt')
@@ -34,5 +52,85 @@ module VivoPsIngest
       phone_statement = RDF::Statement.new(name_statement.subject, RDF::URI.new("http://example.org/prop/phone"), "5555555555")
       differences[:additions].has_statement?(phone_statement).should == true
     end
+
+    it "should retrieve a person record from vivo" do
+      client = UpdatePeople.new
+      results = client.retrieve_person_from_vivo(@ufid)
+      results.size.should == 11
+      check_predicate_value(results, @predicates[:first_name], "Alexander")
+      check_predicate_value(results, @predicates[:last_name], "Rockwell")
+      check_predicate_value(results, @predicates[:middle_name], "H")
+      check_predicate_value(results, @predicates[:prefix_name], "Mr")
+      check_predicate_value(results, @predicates[:work_phone], "3522732590")
+      check_predicate_value(results, @predicates[:work_email], "alexhr@ufl.edu")
+      results.query(:predicate => RDF::URI.new('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')).size.should == 5
+
+      results_2 = results
+    end
+
+    it "should create rdf of their working title from peoplesoft" do
+      dbh = DBI.connect(ENV['mysql_connection'], ENV['mysql_username'], ENV['mysql_password'])
+
+      update_people = UpdatePeople.new
+      graph = update_people.create_work_title_rdf(dbh, @uri, @ufid)
+      check_predicate_value(graph, @predicates[:hr_job_title], "Vivo Local Implementation Support")
+    end
+
+    it "should create rdf of their glid from peoplesoft" do
+      dbh = DBI.connect(ENV['mysql_connection'], ENV['mysql_username'], ENV['mysql_password'])
+
+      update_people = UpdatePeople.new
+      graph = update_people.create_glid_rdf(dbh, @uri, @ufid)
+      check_predicate_value(graph, @predicates[:gatorlink], "alexhr")
+    end
+
+    it "should create rdf of their work_email from peoplesoft" do
+      dbh = DBI.connect(ENV['mysql_connection'], ENV['mysql_username'], ENV['mysql_password'])
+      
+      update_people = UpdatePeople.new
+      graph = update_people.create_work_email_rdf(dbh, @uri, @ufid)
+      check_predicate_value(graph, @predicates[:work_email], "alexhr@ufl.edu")
+    end
+
+    it "should create name rdf from peoplesoft" do
+      dbh = DBI.connect(ENV['mysql_connection'], ENV['mysql_username'], ENV['mysql_password'])
+
+      update_people = UpdatePeople.new
+      graph = update_people.create_name_rdf(dbh, @uri, @ufid)
+      check_predicate_value(graph, @predicates[:first_name], "Alexander")
+      check_predicate_value(graph, @predicates[:last_name], "Rockwell")
+      check_predicate_value(graph, @predicates[:middle_name], "H")
+      check_predicate_value(graph, @predicates[:prefix_name], "Mr")
+    end
+
+    it "should create phone number rdf from peoplesoft" do
+      dbh = DBI.connect(ENV['mysql_connection'], ENV['mysql_username'], ENV['mysql_password'])
+
+      update_people = UpdatePeople.new
+      graph = update_people.create_phone_number_rdf(dbh, @uri, @ufid)
+      check_predicate_value(graph, @predicates[:work_phone], "352.273.2590")
+    end
+
+    # tie everything together
+    it "should create rdf for a person from peoplesoft" do
+      dbh = DBI.connect(ENV['mysql_connection'], ENV['mysql_username'], ENV['mysql_password'])
+
+      update_people = UpdatePeople.new
+      graph = update_people.create_rdf_for_person_in_ps(dbh, @uri, @ufid)
+      check_predicate_value(graph, @predicates[:hr_job_title], "Vivo Local Implementation Support")
+      check_predicate_value(graph, @predicates[:gatorlink], "alexhr")
+      check_predicate_value(graph, @predicates[:work_email], "alexhr@ufl.edu")
+      check_predicate_value(graph, @predicates[:first_name], "Alexander")
+      check_predicate_value(graph, @predicates[:last_name], "Rockwell")
+      check_predicate_value(graph, @predicates[:middle_name], "H")
+      check_predicate_value(graph, @predicates[:prefix_name], "Mr")
+      check_predicate_value(graph, @predicates[:work_phone], "352.273.2590")
+    end
+
+    def check_predicate_value(graph, predicate, expected_value)
+      predicate.nil?.should == false
+      graph.query(:predicate => predicate).first.object.value.should == expected_value
+    end
+    
   end
 end
