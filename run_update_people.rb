6eea0b7fb982e5fb090fa172938f3d2364226048
Alex Rockwell <alexhr@ufl.edu>
2011-02-22 15:55:52 -0500

    Created script to generate additions and removals file.

---
+++ run_update_people.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+require 'conf.rb'
+require 'rdf/isomorphic'
+require 'lib/vivo_ps_ingest/update_people.rb'
+
+update_people = VivoPsIngest::UpdatePeople.new
+differences = update_people.update_people
+
+removal_file = 'removals.nt'
+addition_file = 'additions.nt'
+update_people.serialize_graph(differences[:removals], removal_file)
+update_people.serialize_graph(differences[:additions], addition_file)
